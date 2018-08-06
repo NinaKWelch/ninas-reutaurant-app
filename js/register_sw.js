3bb42f91237c388aa4eb2a1c2f285333d7bacddf
ninakwelch <ninakwelch@gmail.com>
2018-08-06 02:45:18 +0100

    fix: service worker

--- js/register_sw.js
+++ js/register_sw.js
@@ -4,13 +4,12 @@
 
 // register service worker
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+  navigator.serviceWorker.register('./sw.js')
+  .then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
   });
 }
--- sw.js
+++ sw.js
@@ -77,12 +77,11 @@ self.addEventListener('activate', function(event) {
 
 //Return cached responses
 self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			//cache hit - return response
-			return response || fetch(event.request);
-		})
-	);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
 
 
