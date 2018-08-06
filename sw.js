c5de276c59d70b77f10e0b887341021a93cc3491
ninakwelch <ninakwelch@gmail.com>
2018-08-06 03:37:01 +0100

    fix: service worker fetch

--- sw.js
+++ sw.js
@@ -67,8 +67,14 @@ self.addEventListener('activate', function (event) {
 
 //Return cached responses
 self.addEventListener('fetch', function (event) {
-	event.respondWith(caches.match(event.request).then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
+  event.respondWith(
+    caches.match(event.request).then(function (resp) {
+      return resp || fetch(event.request).then(function (response) {
+        return caches.open('v1').then(function (cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
